72201fb791da544470b56820d3c390440a0b7a65
VitaliMay <mvt30@mail.ru>
2026-01-21 16:28:46 +0300

    feat: add basic adaptive

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { signatureScore } from "./js/score.js";
 import { initChangeLang } from "./js/lang.js";
 
 // отменяю контекстное меню при длинном таче
-body.addEventListener('contextmenu', function (event) {
-  event.preventDefault();
-});
+// body.addEventListener('contextmenu', function (event) {
+//   event.preventDefault();
+// });
 
 
 /************************************************** */
