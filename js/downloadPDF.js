4a4f5845a9f9fc60275eff1c8a5c06ec0d59c070
VitaliMay <mvt30@mail.ru>
2026-02-27 21:19:35 +0300

    feat: add spinner

--- js/downloadPDF.js
+++ js/downloadPDF.js
@@ -1,22 +1,33 @@
-import { buttonDownload } from "./variables.js";
+import { buttonDownload, spinner } from "./variables.js";
 import { langState } from "./lang.js";
 
 async function downloadExistingPDF() {
   const lang = langState.isEnglish ? 'rus' : 'eng'
-  const response = await fetch(`./assets/download/CV_VitaliMay_${lang}.pdf`);
-  const pdfBlob = await response.blob(); // получаем Blob из ответа
 
-  // Создаем ссылку для скачивания
-  const url = URL.createObjectURL(pdfBlob);
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = `CV_VitaliMay_${lang}.pdf`; // имя файла при скачивании
-  document.body.appendChild(link);
-  link.click();
+  spinner.classList.add('spinner_active')
 
-  // Очищаем
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
+  try {
+    const response = await fetch(`./assets/download/CV_VitaliMay_${lang}.pdf`);
+    const pdfBlob = await response.blob(); // получаем Blob из ответа
+
+    // Создаем ссылку для скачивания
+    const url = URL.createObjectURL(pdfBlob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = `CV_VitaliMay_${lang}.pdf`; // имя файла при скачивании
+    document.body.appendChild(link);
+    link.click();
+
+    // Очищаем
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+
+  } catch (error) {
+    console.error(error);
+
+  } finally {
+    spinner.classList.remove('spinner_active')
+  }
 }
 
 export const initDownload = () => {
